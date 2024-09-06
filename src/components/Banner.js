1d353fc89d76949c256af975df222d101898e7f7
rhdmswls12 <rhgusdnr1293@naver.com>
2024-09-06 23:43:32 +0900

    이미지 배너 UI 생성

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,7 +1,7 @@
-import instance from "../api/axios";
 import axios from "../api/axios";
 import request from "../api/request";
 import React, { useEffect, useState } from "react";
+import "./Banner.css";
 
 const Banner = () => {
   const [movie, setMovie] = useState([]);
@@ -27,7 +27,33 @@ const Banner = () => {
     setMovie(movieDetail);
     console.log(movieDetail);
   };
-  return <div>Banner</div>;
+
+  const truncate = (str, n) => {
+    return str?.length > n ? str.substring(0, n) + "..." : str;
+  };
+  return (
+    <header
+      className="banner"
+      style={{
+        backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie.backdrop_path})`,
+        backgroundPosition: "top center",
+        backgroundSize: "cover",
+      }}
+    >
+      <div className="banner__contents">
+        <h1 className="banner__title">
+          {movie.title || movie.name || movie.original_name}
+        </h1>
+        <div className="banner__buttons">
+          {movie?.videos?.results[0].key && (
+            <button className="banner__button play">Play</button>
+          )}
+        </div>
+        <p className="banner__description">{truncate(movie.overview, 100)}</p>
+      </div>
+      <div className="banner--fadeBottom" />
+    </header>
+  );
 };
 
 export default Banner;
